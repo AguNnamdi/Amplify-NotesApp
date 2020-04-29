0be5afd076a1f54fd71d67b8c57dfcf34bfe6178
Nnamdi Agu <morgana@Nnamdis-MacBook-Pro.local>
2020-04-29 23:52:24 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,155 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import  { Hub } from '@aws-amplify/core';
+import { DataStore, Predicates } from '@aws-amplify/datastore';
+import { Note } from './models';
+import { withAuthenticator } from 'aws-amplify-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function listNotes(setNotes) {
+  const notes = await DataStore.query(Note, Predicates.ALL);
+  setNotes(notes);
 }
 
-export default App;
+function App() {
+  const [notes, setNotes] = useState([]);
+  const [value, setValue] = useState("");
+  const [id, setId] = useState("");
+  const [displayAdd, setDisplayAdd] = useState(true);
+  const [displayUpdate, setDisplayUpdate] = useState(false);
+  const [displaySearch, setDisplaySearch] = useState(false);
+
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    await DataStore.save(
+      new Note({
+        note: value
+      })
+    );
+    listNotes(setNotes);
+    setValue("")
+  };
+
+  async function handleSearch(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    setDisplaySearch(true);
+    const search = await DataStore.query(Note, c => c.note("contains", value));
+    setNotes(search);
+    setValue("");
+  }
+
+  async function handleDelete(id) {
+    const toDelete = await DataStore.query(Note, id);
+    await DataStore.delete(toDelete);
+  }
+
+  async function handleSelect(note) {
+    setValue(note.note);
+    setId(note.id);
+    setDisplayUpdate(true);
+    setDisplayAdd(false);
+  }
+
+  async function handleUpdate(evt) {
+    evt.preventDefault();
+    evt.stopPropagation();
+    const original = await DataStore.query(Note, id);
+    await DataStore.save(
+      Note.copyOf(original, updated => {updated.note = value} )
+    );
+    listNotes(setNotes);
+    setDisplayAdd(true);
+    setDisplayUpdate(false);
+    setValue("");
+  }
+
+  useEffect(() => {
+    listNotes(setNotes);
+
+    const listener = (data) => {
+      if (data.payload.event === "signOut"){
+        DataStore.clear();
+      }
+    }
+    Hub.listen('auth', listener );
+
+    const subscription = DataStore.observe(Note).subscribe(msg => {
+      listNotes(setNotes);
+    });
+
+    const handleConnectionChange = () => {
+      const condition = navigator.onLine ? "onLine" : "offline";
+      console.log(condition);
+      if (condition === "online") {
+        listNotes(setNotes);
+      }
+    };
+
+    window.addEventListener("online", handleConnectionChange);
+    window.addEventListener("offline", handleConnectionChange)
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="jumbotron jumbotron-fluid bg-dark">
+        <img src={logo} className="App-logo" alt="logo" style={{ height: "150px" }}/>
+      </header>
+      <div className="container">
+        { displayAdd ? (
+          <form>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control form-control-lg" placeholder= "NewNote" aria-label="Note" aria-describedby="basic-addon2" value={value} onChange={e => setValue(e.target.value)} />
+              <div className="input-group-append">
+                <button className="btn btn-warning border border-light text-white font-weight-bold" type="button" onClick={e => { handleSubmit(e); }}>
+                  Add Note
+                </button>
+                <button className="btn btn-warning border border-light text-white font-weight-bold" type="button" onClick={e => { handleSearch(e); }} >
+                  Search
+                </button>
+              </div>
+            </div>
+          </form>
+        ) : null}
+        {displayUpdate ? (
+          <form onSubmit={e => {handleUpdate(e); }}>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control form-control-lg" placeholder= "Update Note" aria-label="Note" aria-describedby="basic-addon2" value={value} onChange={e => setValue(e.target.value)} />
+              <div className="input-group-append">
+                  <button className="btn btn-warning text-white font-weight-bold" type="submit" >
+                    Update Note
+                  </button>
+              </div>
+            </div>
+          </form>
+        ): null}
+      </div>
+
+      <div className="container">
+        {notes.map((item, i) => {
+          return (
+            <div className="alert alert-warning alert-dismissible text-dark show" role="alert">
+              <span key={item.i} onClick={() => handleSelect(item)}>
+                {item.note}
+              </span>
+              <button key={item.i} type="button" className="close" data-dismiss="alert" aria-label="Close" onClick={() => { handleDelete(item.id); listNotes(setNotes); }} >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          );
+        })}
+        {displaySearch ? (
+          <button className="button btn-warning float-right text-white font-weight-bold" onClick={() => {setDisplaySearch(false); listNotes(setNotes); }}>
+            <span aria-hidden="true">Clear Search</span>
+          </button>
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+export default withAuthenticator(App, { includeGreetings: true });
---
+++ src/models/schema.js
@@ -0,0 +1,59 @@
+export const schema = {
+    "models": {
+        "Note": {
+            "name": "Note",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "note": {
+                    "name": "note",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                },
+                "owner": {
+                    "name": "owner",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Notes",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                },
+                {
+                    "type": "auth",
+                    "properties": {
+                        "rules": [
+                            {
+                                "provider": "userPools",
+                                "ownerField": "owner",
+                                "allow": "owner",
+                                "identityClaim": "cognito:username",
+                                "operations": [
+                                    "create",
+                                    "update",
+                                    "delete"
+                                ]
+                            }
+                        ]
+                    }
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "456329cc9479bcfbe489218930ca26eb"
+};
